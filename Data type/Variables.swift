d7903735bee92c560ec169373ce30ad139dd28b8
DevolperPHP <devolperphp@gmail.com>
2021-11-10 01:41:55 +0300

    Update Variables.swift

--- Data type/Variables.swift
+++ Data type/Variables.swift
@@ -1,6 +1,12 @@
 var name = "Mohammed"
 var lastname = "Majid"
 
-print(name + " " + lastname)
+var city : String = "Mosul"
+var age : Int = 18
+var x : String
+
+print(name + " " + lastname + " " + city + " " + age)
+
+
 
 // Outout -> Mohammed Majid

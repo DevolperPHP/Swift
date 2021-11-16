82e0e8e6372d857e50a3c7962d8ac3c98e6e79f7
DevolperPHP <devolperphp@gmail.com>
2021-11-16 16:47:19 +0300

    Update Array.swift

--- Data type/Array.swift
+++ Data type/Array.swift
@@ -1,4 +1,27 @@
-var myArray = ["mohammed","ahmed", "ali"]
+var names = ["mohammed", "ali"]
 
-print(myArray)
-print(myArray[0])
+print(names)
+
+var cities : [String] = ["Baghdad", "Mosuel"]
+
+cities.append("Dhouk")
+
+print("citest fater append : \(cities)")
+
+cities += ["Oman", "Jedah"]
+
+print("cites after add many : \(cities)")
+
+cities.insert("Istanbul", at: 3)
+
+print("cites after insert \(cities)")
+
+cities[4] = "Dubai"
+
+print("cites after replaced : \(cities)")
+
+cities.remove(at: 3)
+
+print("cites after delete : \(cities)")
+
+print(cities.count)

e5aec893624cc1eeba9e0bb60dec1764311cf77e
DevolperPHP <devolperphp@gmail.com>
2021-11-16 17:24:04 +0300

    Create math_operaters.swift

---
+++ math/math_operaters.swift
@@ -0,0 +1,21 @@
+var x = 5
+var y = 3.5
+var z = Double(x) + y
+
+var a = 10, b = 7, c = 5
+
+print("a + b = \(a + b)")
+print("a * b = \(a * b)")
+print("a - b = \(a - b)")
+print("a / b = \(a / b)")
+print("a % b = \(a % b)")
+
+print(a+b*c)
+print((a+b) * c)
+
+
+a += 2
+a -= 2
+a /= 2
+a *= 2
+a %= 2
